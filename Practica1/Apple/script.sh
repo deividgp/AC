9e4c3c081a7f89f4432c9436faa003cacb2265f8
deividgp <dayd4@outlook.com>
2021-10-20 19:36:47 +0200

    Config edited

--- Practica1/Apple/script.sh
+++ Practica1/Apple/script.sh
@@ -1,5 +1,5 @@
-sim-outorder -redir:sim applu.txt -config config.txt ../applu/exe/applu.exe
-sim-outorder -redir:sim crafty.txt -config config.txt ../crafty/exe/crafty.exe
-sim-outorder -redir:sim mesa.txt -config config.txt ../mesa/exe/mesa.exe
-sim-outorder -redir:sim vortex.txt -config config.txt ../vortex/exe/vortex.exe
-sim-outorder -redir:sim vpr.txt -config config.txt ../vpr/exe/vpr.exe
+sim-outorder -redir:sim applu.txt -config config.txt ../applu/exe/applu.exe < ../applu/data/ref/applu.in
+sim-outorder -redir:sim crafty.txt -config config.txt ../crafty/exe/crafty.exe < ../crafty/data/ref/crafty.in
+sim-outorder -redir:sim mesa.txt -config config.txt ../mesa/exe/mesa.exe < ../mesa/data/ref/mesa.in
+sim-outorder -redir:sim vortex.txt -config config.txt ../vortex/exe/vortex.exe < ../vortex/data/ref/vortex.in
+sim-outorder -redir:sim vpr.txt -config config.txt ../vpr/exe/vpr.exe < ../vpr/data/ref/vpr.in
