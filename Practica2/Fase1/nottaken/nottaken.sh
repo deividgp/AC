cf63b29e46ef51412ddcb054e338ae210d740e14
Deividgp <dayd4@outlook.com>
2021-11-27 21:37:37 +0100

    fase1

--- Practica2/Fase1/nottaken/nottaken.sh
+++ Practica2/Fase1/nottaken/nottaken.sh
@@ -1,10 +1,10 @@
 #Applu
-sim-bpred -redir:sim nottakenApplu -fastfwd 50000000 -max:inst 65000000 -bpred nottaken ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
+sim-outorder -redir:sim nottakenApplu -fastfwd 50000000 -max:inst 65000000 -bpred nottaken -mem:width 32 -mem:lat 300 2 ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
 #Crafty
-sim-bpred -redir:sim nottakenCrafty -fastfwd 50000000 -max:inst 65000000 -bpred nottaken ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
+sim-outorder -redir:sim nottakenCrafty -fastfwd 50000000 -max:inst 65000000 -bpred nottaken -mem:width 32 -mem:lat 300 2 ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
 #Mesa
-sim-bpred -redir:sim nottakenMesa -fastfwd 50000000 -max:inst 65000000 -bpred nottaken ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
+sim-outorder -redir:sim nottakenMesa -fastfwd 50000000 -max:inst 65000000 -bpred nottaken -mem:width 32 -mem:lat 300 2 ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
 #Vpr
-sim-bpred -redir:sim nottakenVpr -fastfwd 50000000 -max:inst 65000000 -bpred nottaken ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
+sim-outorder -redir:sim nottakenVpr -fastfwd 50000000 -max:inst 65000000 -bpred nottaken -mem:width 32 -mem:lat 300 2 ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
 #Vortex
-sim-bpred -redir:sim nottakenVortex -fastfwd 50000000 -max:inst 65000000 -bpred nottaken ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
+sim-outorder -redir:sim nottakenVortex -fastfwd 50000000 -max:inst 65000000 -bpred nottaken -mem:width 32 -mem:lat 300 2 ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
