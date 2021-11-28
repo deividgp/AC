aa915c4f5cf2f2ce327ab4352ded25bc5c9c5a90
Deividgp <dayd4@outlook.com>
2021-11-28 09:42:01 +0100

    Gag

--- Practica2/Fase1/gag/gag.sh
+++ Practica2/Fase1/gag/gag.sh
@@ -1,10 +1,30 @@
 #Applu
-sim-outorder -redir:sim gagApplu -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
+sim-outorder -redir:sim gagApplu8 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
+sim-outorder -redir:sim gagApplu32 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 32 5 0 -mem:width 32 -mem:lat 300 2 ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
+sim-outorder -redir:sim gagApplu128 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 128 7 0 -mem:width 32 -mem:lat 300 2 ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
+sim-outorder -redir:sim gagApplu512 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 512 9 0 -mem:width 32 -mem:lat 300 2 ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
+sim-outorder -redir:sim gagApplu2048 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 2048 11 0 -mem:width 32 -mem:lat 300 2 ../../../applu/exe/applu.exe < ../../../applu/data/ref/applu.in
 #Crafty
-sim-outorder -redir:sim gagCrafty -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
+sim-outorder -redir:sim gagCrafty8 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
+sim-outorder -redir:sim gagCrafty32 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 32 5 0 -mem:width 32 -mem:lat 300 2 ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
+sim-outorder -redir:sim gagCrafty128 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 128 7 0 -mem:width 32 -mem:lat 300 2 ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
+sim-outorder -redir:sim gagCrafty512 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 512 9 0 -mem:width 32 -mem:lat 300 2 ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
+sim-outorder -redir:sim gagCrafty2048 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 2048 11 0 -mem:width 32 -mem:lat 300 2 ../../../crafty/exe/crafty.exe < ../../../crafty/data/ref/crafty.in
 #Mesa
-sim-outorder -redir:sim gagMesa -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
+sim-outorder -redir:sim gagMesa8 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
+sim-outorder -redir:sim gagMesa32 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 32 5 0 -mem:width 32 -mem:lat 300 2 ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
+sim-outorder -redir:sim gagMesa128 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 128 7 0 -mem:width 32 -mem:lat 300 2 ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
+sim-outorder -redir:sim gagMesa512 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 512 9 0 -mem:width 32 -mem:lat 300 2 ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
+sim-outorder -redir:sim gagMesa2048 -fastfwd 50000000 -max:inst 65000000 -bpred:2lev 1 2048 11 0 -mem:width 32 -mem:lat 300 2 ../../../mesa/exe/mesa.exe -frames 1000 -meshfile ../../../mesa/data/ref/mesa.in -ppmfile ../../../mesa/data/ref/mesa.ppm
 #Vpr
-sim-outorder -redir:sim gagVpr -fastfwd 50000000 -max:inst 65000000  -bpred 2lev -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
+sim-outorder -redir:sim gagVpr8 -fastfwd 50000000 -max:inst 65000000  -bpred 2lev -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
+sim-outorder -redir:sim gagVpr32 -fastfwd 50000000 -max:inst 65000000  -bpred 2lev -bpred:2lev 1 32 5 0 -mem:width 32 -mem:lat 300 2 ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
+sim-outorder -redir:sim gagVpr128 -fastfwd 50000000 -max:inst 65000000  -bpred 2lev -bpred:2lev 1 128 7 0 -mem:width 32 -mem:lat 300 2 ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
+sim-outorder -redir:sim gagVpr512 -fastfwd 50000000 -max:inst 65000000  -bpred 2lev -bpred:2lev 1 512 9 0 -mem:width 32 -mem:lat 300 2 ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
+sim-outorder -redir:sim gagVpr2048 -fastfwd 50000000 -max:inst 65000000  -bpred 2lev -bpred:2lev 1 2048 11 0 -mem:width 32 -mem:lat 300 2 ../../../vpr/exe/vpr.exe ../../../vpr/data/ref/net.in ../../../vpr/data/ref/arch.in place.out dum.out -nodisp -place_only -init_t 5 -exit_t 0.005 -alpha_t 0.9412 -inner_num 2
 #Vortex                                                                                           
-sim-outorder -redir:sim gagVortex -fastfwd 50000000 -max:inst 65000000 -bpred 2lev -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
+sim-outorder -redir:sim gagVortex8 -fastfwd 50000000 -max:inst 65000000 -bpred 2lev -bpred:2lev 1 8 3 0 -mem:width 32 -mem:lat 300 2 ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
+sim-outorder -redir:sim gagVortex32 -fastfwd 50000000 -max:inst 65000000 -bpred 2lev -bpred:2lev 1 32 5 0 -mem:width 32 -mem:lat 300 2 ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
+sim-outorder -redir:sim gagVortex128 -fastfwd 50000000 -max:inst 65000000 -bpred 2lev -bpred:2lev 1 128 7 0 -mem:width 32 -mem:lat 300 2 ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
+sim-outorder -redir:sim gagVortex512 -fastfwd 50000000 -max:inst 65000000 -bpred 2lev -bpred:2lev 1 512 9 0 -mem:width 32 -mem:lat 300 2 ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
+sim-outorder -redir:sim gagVortex2048 -fastfwd 50000000 -max:inst 65000000 -bpred 2lev -bpred:2lev 1 2048 11 0 -mem:width 32 -mem:lat 300 2 ../../../vortex/exe/vortex.exe ../../../vortex/data/ref/lendian1.raw
