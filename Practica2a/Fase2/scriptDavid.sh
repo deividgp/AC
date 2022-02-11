d70c431369308383a17eda466c44935317daf08f
Deividgp <dayd4@outlook.com>
2022-02-11 01:15:05 +0100

    canvis

--- Practica2a/Fase2/scriptDavid.sh
+++ Practica2a/Fase2/scriptDavid.sh
@@ -1,5 +1,11 @@
-sim-outorder -redir:sim David/1000.txt -config config.txt fitxer_alpha 1000
-sim-outorder -redir:sim David/2000.txt -config config.txt fitxer_alpha 2000
-sim-outorder -redir:sim David/3000.txt -config config.txt fitxer_alpha 3000
-sim-outorder -redir:sim David/4000.txt -config config.txt fitxer_alpha 4000
-sim-outorder -redir:sim David/5000.txt -config config.txt fitxer_alpha 5000
+#sim-outorder -redir:sim David/1000.txt -config config.txt fitxer_alpha 1000
+#sim-outorder -redir:sim David/2000.txt -config config.txt fitxer_alpha 2000
+#sim-outorder -redir:sim David/3000.txt -config config.txt fitxer_alpha 3000
+#sim-outorder -redir:sim David/4000.txt -config config.txt fitxer_alpha 4000
+#sim-outorder -redir:sim David/5000.txt -config config.txt fitxer_alpha 5000
+
+sim-outorder -redir:sim David/modificat/1000.txt -config configModificat.txt fitxer_alpha 1000
+sim-outorder -redir:sim David/modificat/2000.txt -config configModificat.txt fitxer_alpha 2000
+sim-outorder -redir:sim David/modificat/3000.txt -config configModificat.txt fitxer_alpha 3000
+sim-outorder -redir:sim David/modificat/4000.txt -config configModificat.txt fitxer_alpha 4000
+sim-outorder -redir:sim David/modificat/5000.txt -config configModificat.txt fitxer_alpha 5000
--- Practica2a/Fase2/scriptPedro.sh
+++ Practica2a/Fase2/scriptPedro.sh
@@ -1,5 +1,11 @@
-sim-outorder -redir:sim Pedro/1000.txt -config config.txt fitxer_alpha 1000
-sim-outorder -redir:sim Pedro/2000.txt -config config.txt fitxer_alpha 2000
-sim-outorder -redir:sim Pedro/3000.txt -config config.txt fitxer_alpha 3000
-sim-outorder -redir:sim Pedro/4000.txt -config config.txt fitxer_alpha 4000
-sim-outorder -redir:sim Pedro/5000.txt -config config.txt fitxer_alpha 5000
+#sim-outorder -redir:sim Pedro/1000.txt -config config.txt fitxer_alpha 1000
+#sim-outorder -redir:sim Pedro/2000.txt -config config.txt fitxer_alpha 2000
+#sim-outorder -redir:sim Pedro/3000.txt -config config.txt fitxer_alpha 3000
+#sim-outorder -redir:sim Pedro/4000.txt -config config.txt fitxer_alpha 4000
+#sim-outorder -redir:sim Pedro/5000.txt -config config.txt fitxer_alpha 5000
+
+sim-outorder -redir:sim Pedro/modificat/1000.txt -config configModificat.txt fitxer_alpha 1000
+sim-outorder -redir:sim Pedro/modificat/2000.txt -config configModificat.txt fitxer_alpha 2000
+sim-outorder -redir:sim Pedro/modificat/3000.txt -config configModificat.txt fitxer_alpha 3000
+sim-outorder -redir:sim Pedro/modificat/4000.txt -config configModificat.txt fitxer_alpha 4000
+sim-outorder -redir:sim Pedro/modificat/5000.txt -config configModificat.txt fitxer_alpha 5000
